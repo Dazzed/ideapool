47399c4c89718696fda0c2c429809cd3f6cd9c35
Dazzed <sameepsheth@gmail.com>
2018-06-27 17:24:41 +0530

    User Sign In

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :users
+    post 'access_tokens/refresh', to: 'access_tokens#refresh'
+    post 'access_tokens', to: 'access_tokens#signin'
   end
   get 'signup', to: 'users#signup'
+  get 'signin', to: 'users#signin'
 
 end
