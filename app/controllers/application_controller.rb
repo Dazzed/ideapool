d9000e8d0ae30a9776b3dd6cb14ae12ca7003984
Dazzed <sameepsheth@gmail.com>
2018-06-28 11:44:11 +0530

    Refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authenticate_user
+    begin
+      if JsonWebToken.decode(request.headers['x-access-token']) 
+        id = JsonWebToken.decode(request.headers['x-access-token'])[0]["user_id"].to_i
+        @user = User.find(id)
+        if @user.nil?
+          return render json: { errors: e }, status: 401
+        end
+        if @user.jwt != request.headers['x-access-token']
+          return render json: { errors: "Use New Access Token" }, status: :bad_request
+        end
+        if @user.refresh_token.nil?
+          return render json: { errors: "Please Login" }, status: :bad_request
+        end
+      else
+        return render json: { errors: e }, status: 401
+      end
+    rescue => e 
+      render json: { errors: e }, status: 401
+    end
+  end
+
 end
