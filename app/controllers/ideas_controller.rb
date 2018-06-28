64f84c3cb6abae25b05f2dc98cb98c53f64b1949
Dazzed <sameepsheth@gmail.com>
2018-06-28 12:28:32 +0530

    Pagination

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,23 +3,30 @@ class IdeasController < ApplicationController
   before_action :authenticate_user, except: [:index]
 
   def index
+    limit = 10
+    totalPages = 0
     if request.cookies['__idea_access_token'].nil?
-      ideas = Idea.where(public: true)
-      @ideas_props = { name: "Guest", avatar_url: "/assets/user.png", ideas: ideas }
+      ideas = Idea.where(public: true).limit(limit)
+      totalPages = (Idea.where(public: true).count/limit.to_f).ceil
+      @ideas_props = { name: "Guest", avatar_url: "/assets/user.png", ideas: ideas, totalPages: totalPages }
     else
       begin
         id = JsonWebToken.decode(request.cookies['__idea_access_token'])[0]["user_id"].to_i
         u = User.find(id)
         if u.nil? || u.jwt != request.cookies['__idea_access_token'] || u.refresh_token.nil?
-          ideas = Idea.where(public: true)
-          @ideas_props = { name: "Guest", avatar_url: "/assets/user.png", ideas: ideas }
+          ideas = Idea.where(public: true).limit(limit)
+          totalPages = (Idea.where(public: true).count/limit.to_f).ceil
+          @ideas_props = { name: "Guest", avatar_url: "/assets/user.png", ideas: ideas, totalPages: totalPages }
           return 
         end
-        ideas = u.ideas
-        @ideas_props = { name: u.name, avatar_url: u.avatar_url, ideas: ideas }
+        ideas = u.ideas.limit(limit)
+        totalPages = (u.ideas.count/limit.to_f).ceil
+
+        @ideas_props = { name: u.name, avatar_url: u.avatar_url, ideas: ideas, totalPages: totalPages }
       rescue Exception => e 
-        ideas = Idea.where(public: true)
-        @ideas_props = { name: "Guest", avatar_url: "/assets/user.png", ideas: ideas }
+        ideas = Idea.where(public: true).limit(limit)
+        totalPages = (Idea.where(public: true).count/limit.to_f).ceil
+        @ideas_props = { name: "Guest", avatar_url: "/assets/user.png", ideas: ideas, totalPages: totalPages }
         return
       end
     end
